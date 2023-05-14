b3c93e347406d4b2f41d74faf9252fc2879b4cb7
pranta.zicore <pranta.zicore@gmail.com>
2023-05-14 18:22:17 +0600

    add code for slice & loop

--- cards/main.go
+++ cards/main.go
@@ -6,8 +6,18 @@ func main() {
 	//var card string = "Ace of spades" // declare variable
 	//card := "Ace of spades"   // when initializing a value with a new variable
 	//card = "five of diamonds" // assigning another value
-	card := newCard() // call a function
-	fmt.Println(card)
+	//card := newCard() // call a function
+	//fmt.Println(card)
+
+	// slice
+	cards := []string{"hello", "hi", newCard()}
+	fmt.Println(cards)
+	cards = append(cards, "Six of hearts")
+
+	//	looping through the cards
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 // function with return type
