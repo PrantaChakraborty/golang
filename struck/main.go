cfcfee98fe3ad7867f09cd28d78d6a7c13e534f0
pranta.zicore <pranta.zicore@gmail.com>
2023-05-22 18:27:52 +0600

    add code for struck

--- struck/main.go
+++ struck/main.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+type person struct {
+	firstName string
+	lastname  string
+}
+
 func main() {
 
 	//map implementation
-	var colors map[string]string
+	//var colors map[string]string
 	//colors := make(map[string]string)
 	//colors := map[string]string{
 	//	"red":   "#ff000",
 	//	"green": "#ffdd9",
 	//}
-	colors["white"] = "#ffff"
-	fmt.Println(colors)
+	//colors["white"] = "#ffff"
+	//fmt.Println(colors)
 	//fmt.Println(colors["velvet"])
+	alex := person{firstName: "Alex", lastname: "Anderson"}
+	fmt.Println(alex)
 }
